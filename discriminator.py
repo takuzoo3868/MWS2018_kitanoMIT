915667f665a20895c6cfacb2e0064af40f97f64e
tubutubucorn <33154559+tubutubucorn@users.noreply.github.com>
2018-10-10 18:34:18 +0900

    implement VAT

--- discriminator.py
+++ discriminator.py
@@ -1,35 +1,57 @@
-import numpy as np
+import numpy as np, tensorflow as tf
 from functools import reduce
-from keras.models import Sequential
-from keras.layers import Input, Bidirectional, SimpleRNN, GRU, LSTM, Dense, Activation, BatchNormalization
+from keras.layers import Bidirectional, SimpleRNN, GRU, LSTM, Dense, Activation, BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
+from keras.engine.topology import Input
+from keras.engine.training import Model
 from keras.utils.generic_utils import to_list
-#from keras.engine.topology import Input
-#from keras.engine.training import Model
-from keras.models import Model
 from keras import backend as K
-import tensorflow as tf
+
 
 class BasicModel(object): 
-    def build(self, input_shape, bidirectional=True, vat=True):
-        
+    def build(self, input_shape, rnn_type='RNN', bidirectional=True, vat=True):
+        """ build model
+        :param input_shape: shape=(number of input rows, 1)
+        :param rnn_type: select 'RNN' or 'GRU' or 'LSTM'
+        :param bidirectional: use_flag for Bidirectional rnn
+        :param vat: use_flag for VAT
+        :return: self
+        """
+        self.input_shape = input_shape
         input_layer = Input(self.input_shape)
-        output_layer = self.core_data_flow(input_layer, bidirectional)
+        output_layer = self.core_data_flow(input_layer, rnn_type, bidirectional)
         if vat:
             self.model = VATModel(input_layer, output_layer).setup_vat_loss()
         else:
             self.model = Model(input_layer, output_layer)
         return self
-    def core_data_flow(self, input_layer, bidirectional):
+    
+    def core_data_flow(self, input_layer, rnn_type, bidirectional):
+        """ build rnn model
+        :param input_layer: required for Model()
+        :param rnn_type: select 'RNN' or 'GRU' or 'LSTM'
+        :param bidirectional: use_flag for Bidirectional rnn
+        :return: layer
+        """
         x = Dense(160)(input_layer)
         x = BatchNormalization()(x)
         x = LeakyReLU()(x)
         if bidirectional:
-            x = Bidirectional(SimpleRNN(256))(x)
+            if rnn_type == 'RNN':
+                x = Bidirectional(SimpleRNN(256))(x)
+            elif rnn_type == 'GRU':
+                x = Bidirectional(GRU(256))(x)
+            elif rnn_type == 'LSTM':
+                x = Bidirectional(LSTM(256))(x)
         else:
-            x = SimpleRNN(256)(x)
+            if rnn_type == 'RNN':
+                x = SimpleRNN(256)(x)
+            elif rnn_type == 'GRU':
+                x = GRU(256)(x)
+            elif rnn_type == 'LSTM':
+                x = LSTM(256)(x)
         x = BatchNormalization()(x)
         x = LeakyReLU()(x)
         x = Dense(1)(x)
@@ -37,15 +59,25 @@ class BasicModel(object):
         return x
     
     def train(self, X_train, X_test, y_train, y_test, batch_size=128, epochs=100, early_stop=False):
-        self.model.compile(loss='binary_crossentropy',
+        """ train rnn model
+        :param X_train, X_test, y_train, y_test: X is feature vectol. y is label
+        :param batch_size: onece per training size
+        :param epochs: number of iterations
+        :param early_stopinput_layer: use_flag for EarlyStopping
+        :return: history data
+        """
+        self.model.compile(loss='binary_crossentropy',#'categorical_crossentropy',
                            optimizer=Adam(0.0001, beta_1=0.5, beta_2=0.9), 
                            metrics=['accuracy'])
         np.random.seed(1337)  # for reproducibility
         if early_stop:
             early_stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
-            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test), callbacks=[early_stopping])
+            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
+                                  validation_data=(X_test, y_test), callbacks=[early_stopping])
         else:
-            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test)) 
+            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
+                                  validation_data=(X_test, y_test)) 
+        
     def predict(self, X):
         return self.model.predict(X)
     def evaluate(self, X, y):
@@ -65,148 +97,48 @@ class BasicModel(object):
         score_dict['TPR'] = cm[1, 1] / float(cm[1, 0] + cm[1, 1])
         score_dict['FPR'] = cm[0, 1] / float(cm[0, 0] + cm[0, 1])
         return score_dict
-
-class _RNN(BasicModel):
-    def __init__(self, bidirectional=True, input_shape=(None,None)):
-        self.input_shape = input_shape
-        self.model = Sequential()
-        self.model.add(Dense(160))
-        self.model.add(BatchNormalization())
-        self.model.add(LeakyReLU())
-        if bidirectional:
-            self.model.add(Bidirectional(SimpleRNN(256)))
-        else:
-            self.model.add(SimpleRNN(256))
-        self.model.add(BatchNormalization())
-        self.model.add(LeakyReLU())
-        self.model.add(Dense(2))
-        self.model.add(Activation('sigmoid'))
-    def train(self, X_train, X_test, y_train, y_test, batch_size=128, epochs=100, vat=True,
-              early_stop=False):
-        orig_loss_func = loss_func = 'categorical_crossentropy'
-        if vat:
-            loss_func = self.loss_with_vat_loss(loss_func)
-            self.model.compile(loss=loss_func, 
-                               optimizer=Adam(0.0001, beta_1=0.5, beta_2=0.9), 
-                               metrics=['accuracy'])
-            # train, test ともに y, X を横に連結したデータを作ります
-            yX_train = np.concatenate([y_train.reshape(1,y_train.shape[0]).T, X_train.reshape(X_train.shape[0], -1)], axis=1)
-            yX_test = np.concatenate([y_test.reshape(1,y_test.shape[0]).T, X_test.reshape(X_test.shape[0], -1)], axis=1)
-            if early_stop:
-                early_stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
         
-                # 普通に学習
-                self.model.fit(X_train, yX_train, batch_size=batch_size, epochs=epochs,
-                           verbose=1, validation_data=(X_test, yX_test), callbacks=[early_stopping])
-            else :
-                self.model.fit(X_train, yX_train, batch_size=batch_size, epochs=epochs,
-                           verbose=1, validation_data=(X_test, yX_test))
-            # 変則的なLossFunctionを与えたので、普通のLossFunctionに変更して再びcompileしないと、evaluate()が失敗します
-            self.model.compile(loss=orig_loss_func, optimizer=Adam(0.0001, beta_1=0.5, beta_2=0.9), metrics=['accuracy'])
-        else:
-            self.model.compile(loss=loss_func, optimizer=Adam(0.0001, beta_1=0.5, beta_2=0.9), metrics=['accuracy'])
-            if early_stop:
-                early_stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
-            
-                self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-                           verbose=1, validation_data=(X_test, y_test), callbacks=[early_stopping])
-            else:
-                self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-                           verbose=1, validation_data=(X_test, y_test))
-    def loss_with_vat_loss(self, original_loss_func, eps=1, xi=10, ip=1):
-        def with_vat_loss(yX_train, y_pred):
-            nb_output_classes = y_pred.shape[1]
-            y_true = yX_train[:, :nb_output_classes]
+class VATModel(Model):
+    """ VAT (Virtual Adversarial Training)
+        refored https://qiita.com/mokemokechicken/items/b3cb3d65b6876ccf1a5b
+    """
+    _vat_loss = None
 
-            # VAT
-            #print (type(yX_train[:, nb_output_classes:]))
-            X_train = tf.reshape(yX_train[:, nb_output_classes:], (-1, ) + self.input_shape)
-            #X_train = yX_train[:, nb_output_classes:].reshape((-1, ) + self.input_shape)
-            d = K.random_normal(X_train.shape)
-
-            for _ in range(ip):
-                y = self.core_layers(X_train + self.normalize_vector(d) * xi)
-                kld = K.sum(self.kld(y_pred, y))
-                d = K.stop_gradient(K.gradients(kld, [d])[0])  # stop_gradient is important!!
-
-            y_perturbation = self.core_layers(X_train + self.normalize_vector(d)*eps)
-            kld = self.kld(y_pred, y_perturbation)
-            return original_loss_func(y_pred, y_true) + kld
-        return with_vat_loss
-
+    def setup_vat_loss(self, eps=1, xi=10, ip=1):
+        self._vat_loss = self.vat_loss(eps, xi, ip)
+        return self
+    
+    @property
+    def losses(self):
+        losses = super(self.__class__, self).losses
+        if self._vat_loss is not None:
+            losses += [self._vat_loss]
+        return losses
+    
+    def vat_loss(self, eps, xi, ip):
+        normal_outputs = [K.stop_gradient(x) for x in to_list(self.outputs)]
+        d_list = [K.random_normal(tf.shape(x)) for x in self.inputs]
+        for _ in range(ip):
+            new_inputs = [x + self.normalize_vector(d)*xi for (x, d) in zip(self.inputs, d_list)]
+            new_outputs = to_list(self.call(new_inputs))
+            klds = [K.sum(self.kld(normal, new)) for normal, new in zip(normal_outputs, new_outputs)]
+            kld = reduce(lambda t, x: t+x, klds, 0)
+            d_list = [K.stop_gradient(d) for d in K.gradients(kld, d_list)]
+        new_inputs = [x + self.normalize_vector(d) * eps for (x, d) in zip(self.inputs, d_list)]
+        y_perturbations = to_list(self.call(new_inputs))
+        klds = [K.mean(self.kld(normal, new)) for normal, new in zip(normal_outputs, y_perturbations)]
+        kld = reduce(lambda t, x: t + x, klds, 0)
+        return kld
+    
     @staticmethod
     def normalize_vector(x):
         z = K.sum(K.batch_flatten(K.square(x)), axis=1)
         while K.ndim(z) < K.ndim(x):
-            z = K.expand_dims(z, dim=-1)
+            z = K.expand_dims(z, axis=-1)
         return x / (K.sqrt(z) + K.epsilon())
-
+    
     @staticmethod
     def kld(p, q):
         v = p * (K.log(p + K.epsilon()) - K.log(q + K.epsilon()))
         return K.sum(K.batch_flatten(v), axis=1, keepdims=True)
-
-                 
-class _GRU(BasicModel):
-    def __init__(self, bidirectional=True, dropout=True, vat=True):
-        self.model = Sequential()
-        self.model.add(Dense(160))
-        self.model.add(BatchNormalization())
-        self.model.add(LeakyReLU())    
-        if bidirectional:
-            self.model.add(Bidirectional(GRU(256)))
-        else:
-            self.model.add(GRU(256))
-        self.model.add(BatchNormalization())
-        self.model.add(LeakyReLU())
-        self.model.add(Dense(1))
-        self.model.add(Activation('sigmoid'))
-        if vat:
-            self.model = VATModel(self.model).setup_vat_loss()
-        else:
-            self.model = Model(self.model)
-        self.model.compile(loss='binary_crossentropy',
-                           optimizer=Adam(0.0001, beta_1=0.5, beta_2=0.9), 
-                           metrics=['accuracy'])
-    def train(self, X_train, X_test, y_train, y_test, batch_size=128, epochs=100,
-              early_stop=False):
-        if early_stop:
-            early_stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
-            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-                                  validation_data=(X_test, y_test), callbacks=[early_stopping])
-        else:
-            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, 
-                                  validation_data=(X_test, y_test)) 
-
-class _LSTM(BasicModel):
-    def __init__(self, bidirectional=True, dropout=True, vat=True):
-        self.model = Sequential()
-        self.model.add(Dense(160))
-        self.model.add(BatchNormalization())
-        self.model.add(LeakyReLU())
-        if bidirectional:
-            self.model.add(Bidirectional(LSTM(256)))
-        else:
-            self.model.add(LSTM(256))
-        self.model.add(BatchNormalization())
-        self.model.add(LeakyReLU())
-        self.model.add(Dense(1))
-        self.model.add(Activation('sigmoid'))
-        if self.vat:
-            self.model = VATModel(self.model).setup_vat_loss()
-        else:
-            self.model = Model(self.model)
-        self.model.compile(loss='binary_crossentropy',
-                           optimizer=Adam(0.0001, beta_1=0.5, beta_2=0.9), 
-                           metrics=['accuracy'])
-    def train(self, X_train, X_test, y_train, y_test, batch_size=128, epochs=100, 
-              early_stop=False):
-        if early_stop:
-            early_stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
-            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-                                  validation_data=(X_test, y_test), callbacks=[early_stopping])
-        else:
-            return self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-                                  validation_data=(X_test, y_test)) 
-        
 
--- test.py
+++ test.py
@@ -4,12 +4,17 @@
 # In[1]:
 
 
-import pandas as pd
+import sklearn, numpy as np, pandas as pd, tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from keras.utils import np_utils
+from keras.backend import tensorflow_backend
+import discriminator
 
-
-
-
-
+### if use gpu, please comment out 
+#config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
+#session = tf.Session(config=config)
+#tensorflow_backend.set_session(session)
 
 
 # In[2]:
@@ -29,7 +34,6 @@ def load_kddi_data(data_path):
     # 正常なデータを0, 悪意のあるデータを1とする
     labels = dataset["label"]
     labels = labels.replace({"^.*normal.*":0,"^(?!normal).*$":1}, regex=True)
-
     # 文字列とラベルを取り除く
     drop_columns = ["protocol_type", "service", "flag", "label"]
     return dataset.drop(drop_columns, axis=1), labels
@@ -58,146 +62,109 @@ def load_converted_data(data_path):
 
 # In[4]:
 
-# Load KDDIdata
-#X_kddi, y_kddi = load_kddi_data('./dataset/kddcup99/kddcup.data_10_percent')
-X_kddi, y_kddi = load_kddi_data('./dataset/kddcup99/kddcup.data')
-#X_kddi
+
+# Load KDDI Data
+X_kddi, y_kddi = load_kddi_data('./kddcup99/kddcup.data_10_percent')
+#X_kddi, y_kddi = load_kddi_data('./kddcup99/kddcup.data')
 
 
 # In[5]:
 
 
-load_converted_path = './dataset/trafAld.list'
+# Load Converted BOS Data
+load_converted_path = './BOS_2014/c11/trafAld.list'
 with open(load_converted_path, mode='r') as f:
     s = f.read().replace(' ', ',')
 with open(load_converted_path, mode='w') as f:
     f.write(s)
-    
 X_conv = load_converted_data(load_converted_path)
-#X_conv
 
 
 # In[6]:
 
 
-import sklearn
-import numpy as np
-import pandas as pd
-
-from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score
-
-#pred = KMeans(n_clusters=2, init='random', n_init=10, n_jobs=2).fit_predict(X_kddi)
-#acc = accuracy_score(y_kddi, pred)
-#print ("Accuracy: {:.3}%".format(acc*100))
+scaler = MinMaxScaler(feature_range=(0, 1))  
+#X = scaler.fit_transform(X_kddi)
+X = np.array(X_kddi.values)
+X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+y = np.array(y_kddi.tolist())
+#X_conv_test = scaler.fit_transform(X_conv)
+X_conv_test = np.array(X_conv.values)
+X_conv_test = np.reshape(X_conv_test, (X_conv_test.shape[0], X_conv_test.shape[1], 1))
 
 
 # In[7]:
 
 
-import sklearn
-import numpy as np
-import pandas as pd
-import discriminator
-from sklearn.datasets import load_digits
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=123)
 
-
-# In[8]:
-
-
-scaler = MinMaxScaler(feature_range=(0, 1))  
-#X = scaler.fit_transform(X_kddi)
-#X = pd.DataFrame.from_records(X_kddi)
-X = np.array(X_kddi.values)
-X = np.reshape(X, (X.shape[0], X.shape[1], 1))
-y = np.array(y_kddi.tolist())
-#X_conv_test = scaler.fit_transform(X_conv)
-#X_conv_test = pd.DataFrame.from_records(X_conv_test)
-X_conv_test = np.array(X_conv.values)
-X_conv_test = np.reshape(X_conv_test, (X_conv_test.shape[0], X_conv_test.shape[1], 1))
-
-
-# In[9]:
-
-
-import discriminator
-
-from keras.utils import np_utils
-
-
-# In[10]:
-
-
-my_tsize = .4 # 40%
-my_seed = 123
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=my_tsize, random_state=my_seed)
-SAMPLE_SIZE=0
+SAMPLE_SIZE = 0
 if SAMPLE_SIZE:
     X_train = X_train[:SAMPLE_SIZE]
     y_train = y_train[:SAMPLE_SIZE]
     X_test = X_test[:SAMPLE_SIZE]
     y_test = y_test[:SAMPLE_SIZE]
 
-y_train = np_utils.to_categorical(y_train, 2)
-y_test = np_utils.to_categorical(y_test, 2)
-
-
-# In[11]:
-
-
-rnn_type = ['RNN', 'GRU', 'LSTM']
-resoult = []
-epochs = 100
-#clf = discriminator.BasicModel().build(input_shape=(5,5), bidirectional=False, vat=True)
-clf = discriminator._RNN(bidirectional=True, input_shape=X_train.shape)
-resoult.append(clf.train(X_train, X_test, y_train, y_test, batch_size=128, epochs=epochs, vat=False, early_stop=False))
-#score = clf.evaluate(X_test, y_test)#, verbose=0)
-score = clf.model.evaluate(X_test, y_test, verbose=0)
-print("finish: use_dropout, use_vat: score=%s, accuracy=%s" % (score[0], score[1]))
-
-
-#clf = discriminator._GRU(bidirectional=True)
-#resoult.append(clf.train(X_train, X_test, y_train, y_test, epochs=epochs))
-#clf = discriminator._LSTM(bidirectional=True)
-#resoult.append(clf.train(X_train, X_test, y_train, y_test, epochs=epochs))
-
-
-# In[12]:
-
-
-sco = clf.model.predict(X_conv_test)
-print (sco)
+#y_train = np_utils.to_categorical(y_train, 2)
+#y_test = np_utils.to_categorical(y_test, 2)
 
 
 # In[ ]:
 
 
-#x = range(epochs)
-#for i in range(len(resoult)):
-#    plt.plot(x, resoult[i].history['acc'], label=opt[i])
-#plt.title("binary train accuracy")
-#plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#plt.show()
-#for i in range(len(resoult)):
-#    plt.plot(x, resoult[i].history['val_acc'], label=opt[i])
-#plt.title("binary test accuracy")
-#plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#plt.show()
-#for i in range(len(resoult)):
-#    plt.plot(x, resoult[i].history['loss'], label=opt[i])
-#plt.title("binary train loss")
-#plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#plt.ylim([0,1])
-#plt.show()
-#for i in range(len(resoult)):
-#    plt.plot(x, resoult[i].history['val_loss'], label=opt[i])
-#plt.title("binary test loss")
-#plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#plt.ylim([0,1])
-#plt.show()
+rnn_type = ['RNN', 'GRU', 'LSTM']
+training_resoult = []
+predict_resoult = []
+batch_size = 128
+epochs = 100
+base = discriminator.BasicModel()
+
+for i in rnn_type:
+    clf = base.build(input_shape=(X_train.shape[1], 1), rnn_type=i, bidirectional=True, vat=True)
+    training_resoult.append(clf.train(X_train, X_test, y_train, y_test, batch_size=batch_size,
+                                      epochs=epochs, early_stop=False))
+    predict_resoult.append(np.round(clf.model.predict(X_conv_test)))
+
+#score = clf.evaluate(X_test, y_test)#, verbose=0)
+#score = clf.model.evaluate(X_test, y_test, verbose=0)
+#print("finish: use_dropout, use_vat: score=%s, accuracy=%s" % (score[0], score[1]))
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+x = range(epochs)
+for i in range(len(training_resoult)):
+    plt.plot(x, training_resoult[i].history['acc'], label=rnn_type[i])
+plt.title("binary train accuracy")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+for i in range(len(training_resoult)):
+    plt.plot(x, training_resoult[i].history['val_acc'], label=rnn_type[i])
+plt.title("binary test accuracy")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+for i in range(len(training_resoult)):
+    plt.plot(x, training_resoult[i].history['loss'], label=rnn_type[i])
+plt.title("binary train loss")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.ylim([0,1])
+plt.show()
+for i in range(len(training_resoult)):
+    plt.plot(x, training_resoult[i].history['val_loss'], label=rnn_type[i])
+plt.title("binary test loss")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.ylim([0,1])
+plt.show()
+
+
+# In[ ]:
+
+
+predict_resoult[0].hist(alpha=.5,label=0,bins=40)
+predict_resoult[0].hist(alpha=.5,label=1,bins=40)
+plt.legend()
 
